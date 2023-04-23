c9ad1a7b2ee598cbc9ed692708f48561c933cd32
IvanSPb85 <Vanya85spb@yandex.ru>
2023-04-23 21:14:47 +0300

    feat: Item, ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Builder;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Builder
 public class ItemDto {
+    private long id;
+    @Max(50)
+    @NotBlank
+    private String name;
+    @Max(200)
+    private String description;
+    @NotNull
+    private boolean available;
+    private long request;
 }

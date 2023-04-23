1d9988d00675c9a085c203c746fd3a42860e1dc3
IvanSPb85 <Vanya85spb@yandex.ru>
2023-04-23 11:55:09 +0300

    feat: UserMapper

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -5,12 +5,10 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Null;
 
 @Data
 @AllArgsConstructor
 public class UserDto {
-    @Null
     private long id;
     private String name;
     @NotBlank(message = "email не может быть пустым.")

dc26f6940fee4cc6931481a9793c69cc29c83c97
IvanSPb85 <Vanya85spb@yandex.ru>
2023-04-23 10:51:54 +0300

    feat: model User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
 public class User {
+    private long id;
+    private String name;
+    @NotBlank(message = "email не может быть пустым.")
+    @Email(message = "Некорректный email.")
+    private String email;
 }

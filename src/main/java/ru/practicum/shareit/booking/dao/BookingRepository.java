feb0ef83d01a3f057e2fa9d4f5e4b87b8fded4e5
IvanSPb85 <Vanya85spb@yandex.ru>
2023-06-02 13:22:20 +0300

    feat: add IntegrationTests

--- src/main/java/ru/practicum/shareit/booking/dao/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/dao/BookingRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.booking.dao;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.constant.Status;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findAllByBookerIdOrderByStartDesc(long userId, Pageable pageable);
 
